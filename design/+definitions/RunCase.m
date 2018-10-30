9a7a4783e7118741fb916ac2ecc7c420a9aaea04
skilkis <sankilkis@msn.com>
2018-10-30 15:11:39 +0100

    Optimization is finished, next \n step is to create an XDSM if \n there is time

--- design/+definitions/RunCase.m
+++ design/+definitions/RunCase.m
@@ -1,39 +1,81 @@
-classdef RunCase
+classdef RunCase < handle
     %RUNCASE Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
         x0              % Initial Design Vector
         N               % Number of Stages
-        settings        % fmincon settings
+        data            % Design Parameters
+        options         % fmincon settings
     end
     
-    properties
+    properties (SetAccess = private, GetAccess = private)
+        cache = struct();
+    end
+    
+    properties (SetAccess = private)
+        turbine
+        x_final
+        converged = false
     end
     
     methods
-        function obj = RunCase(N, x0, settings, data)
+        function obj = RunCase(N, x0, options, data)
             %RUNCASE Construct an instance of this class
             %   Detailed explanation goes here
             obj.N = N;
             obj.x0 = x0;
-            obj.settings = settings;
-        end
-        
-        function optimum = optimize(obj)
+            obj.options = options;
+            obj.data = data;
             
+            %Instantiating a Turbine
+            obj.cache.x = obj.x0;
+            Turbine = @definitions.Turbine;
+            obj.cache.turbine = Turbine(obj.data.inflow,...
+                                        x0.psi,...
+                                        x0.phi,...
+                                        x0.R, obj.N,...
+                                        data.RPM,...
+                                        data.PI);
+        end
+         
+        function [c,ceq] = constraints(obj, x)
+            ceq = [];
+            obj.get_turbine(x);
+            % Peripheral Speed Constraint
+            c1 = obj.cache.turbine.stages{1,1}.U - 750;
+%             c2 = obj
+            c = c1;
+        end
+%         
+        function fval = objective(obj, x)
+            obj.get_turbine(x);
+            fval = - obj.cache.turbine.eta_p / obj.cache.turbine.area;
         end
         
-        function [c,ceq] = constraints(obj);
+        function obj = optimize(obj)
+            [x_out, ~] = fmincon(@obj.objective, obj.x0.data,...
+                                 [],[],[],[],obj.x0.lb,obj.x0.ub,...
+                                 @obj.constraints,obj.options);
+            obj.x_final = x_out;
+            obj.get_turbine(x_out) % Final access to ensure convergence
+            obj.turbine = obj.cache.turbine;
+            obj.converged = true;
         end
         
-        function [fval] = objective(obj);
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function obj = get_turbine(obj,x)
+            if obj.cache.x.data == x
+                obj.cache.turbine = obj.cache.turbine;
+            else
+                obj.cache.x.data = x;
+                Turbine = @definitions.Turbine;
+                obj.cache.turbine = Turbine(obj.data.inflow,...
+                                            obj.cache.x.psi,...
+                                            obj.cache.x.phi,...
+                                            obj.cache.x.R, obj.N,...
+                                            obj.data.RPM,...
+                                            obj.data.PI);
+            end
         end
     end
 end
--- design/main.m
+++ design/main.m
@@ -1,12 +1,11 @@
-%%
 clc
 clear all
 close all
 
 %% Imports
 FlowCondition = @definitions.FlowCondition;
-Turbine = @definitions.Turbine;
 DesignVector = @definitions.DesignVector; % TODO use this to make
+RunCase = @definitions.RunCase;
 
 %% Provided Parameters in Assignment III
 % global psi phi R N RPM PI
@@ -22,17 +21,79 @@ data.RPM = 35e3;         % Revolutions Per Minute [rev/min]
 data.PI = 9.3;           % Expansion Ratio [-]
                    
 %% Instantiating Design Vector
-x = DesignVector({'psi', 1.5, 0.5, 3.0;
+x0 = DesignVector({'psi', 1.5, 0.5, 3.0;
                   'phi', 1.2, 0.4, 1.3;
                   'R', 0.5, 0.2, 0.6});
+              
+%% Optimizer Settings
+options.Display         = 'iter-detailed';
+options.Algorithm       = 'sqp';
+options.FunValCheck     = 'off';
+options.DiffMinChange   = 1e-3;         % Minimum change while gradient searching
+options.DiffMaxChange   = 1e-2;         % Maximum change while gradient searching
+options.TolCon          = 1e-6;         % Maximum difference between two subsequent constraint vectors [c and ceq]
+options.TolFun          = 1e-6;         % Maximum difference between two subsequent objective value
+options.TolX            = 1e-6;         % Maximum difference between two subsequent design vectors
 
-%% Instantiating Turbine
-turbine = Turbine(inflow, psi, phi, R, N, RPM, PI);
-turbine.plotEnthalpy();
-turbine.plotVelocityDiagram();
-turbine.plotFlowPath();
+options.MaxIter         = 100;           % Maximum iterations
+              
+%% Defining Run-case for Number of Stages from 1-10
+tic
+N=10;
+areas = zeros(N, 1);
+etas = zeros(N,1);
+runs = {};
+for i = 1:N
+    run = RunCase(i, x0, options, data);
+    run.optimize();
+    runs{i} = run;
+    areas(i) = run.turbine.area;
+    etas(i) = run.turbine.eta_p;
+end
+toc
+
+%% Plotting Most Efficient Turbine
+[M,I] = max(etas);
+opt_turbine = runs{1,I}.turbine;
+fprintf(['Solver Converged! Polytropic Efficiency = %0.4f \n'...
+         'N = %d, psi = %0.2f, phi = %0.2f, R = %0.2f'],...
+          M, I, opt_turbine.psi, opt_turbine.phi, opt_turbine.R)
+% opt_turbine.plotVelocityDiagram();
+% opt_turbine.plotFlowPath();
+% opt_turbine.plotEnthalpy();
+
+
+%% Plotting Efficiency Figure
+f = figure('Name', 'StagesEta');
+grid on; grid minor; hold on;
+plot(1:N, etas, 'LineWidth', 1.5, 'Marker', 'o', 'MarkerFaceColor', 'w')
+x = xlabel('Number of Stages $\left[-\right]$');
+y = ylabel('Polytropic Efficiency, $\eta_p \left[-\right]$');
+t = title('Polytropic Efficiency as a Function of Stage Count');
+
+prop_vector = [x, y, t];
+property_cell = {'Interpreter', 'FontSize'};
+[value_cell{1:length(prop_vector), 1}] = deal('latex');
+[value_cell{1:length(prop_vector), 2}] = deal(12);
+set(prop_vector, property_cell, value_cell)
+f.GraphicsSmoothing = 'on';
+utilities.savefig(f)
+
+%% Plotting Cross Sectional Area Figure
+f = figure('Name', 'StageAreas');
+grid on; grid minor; hold on;
+plot(1:N, areas, 'LineWidth', 1.5, 'Marker', 'o', 'MarkerFaceColor', 'w')
+x = xlabel('Number of Stages $\left[-\right]$');
+y = ylabel('Cross-Sectional Area $\left[\mathrm{m}^2\right]$');
+t = title('Turbine Area as a Function of Stage Count');
+
+prop_vector = [x, y, t];
+property_cell = {'Interpreter', 'FontSize'};
+[value_cell{1:length(prop_vector), 1}] = deal('latex');
+[value_cell{1:length(prop_vector), 2}] = deal(12);
+set(prop_vector, property_cell, value_cell)
+f.GraphicsSmoothing = 'on';
+utilities.savefig(f)
 
 %% Clearing Handles
-clear FlowCondition Turbine
-% % 
-% isa(inflow, 'definitions.FlowCondition')
+clear FlowCondition Turbine Design Vector RunCase
